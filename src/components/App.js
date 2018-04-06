615e037773a52b3ac74463a246cbe76c31a05b32
asher978 <ashershaheen@gmail.com>
2018-04-06 11:04:08 -0400

    conditional rendering in case neavigator fails to get users location

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
 import '../styles/app.css';
 import { Map, Marker, Popup, TileLayer, Tooltip } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      lat: null,
-      lng: null,
-      zoom: 13,
-      posReceived: false
+      lat: 40.7128,
+      lng: -74.0060,
+      zoom: 14,
+      posReceived: true
     }
   };
 
   componentDidMount() {
+    console.log("CDM")
     this.getPosition();
   }
 
-  getPosition () {
+  getPosition = () => {
     navigator.geolocation.getCurrentPosition((pos) => {
+      console.log(pos)
       if(pos.coords) {
         this.setState({
           lat: pos.coords.latitude,
           lng: pos.coords.longitude,
           posReceived: true,
         })
+      } else {
+        this.setState({ posReceived: true })
       }
     })
-    }
+  }
 
   renderMap () {
     const { lat, lng, zoom } = this.state;
@@ -39,14 +44,22 @@ class App extends Component {
               attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position}>
+
+            <MarkerClusterGroup>
+              <Marker position={[49.8397, 24.0297]} />
+              <Marker position={[52.2297, 21.0122]} />
+              <Marker position={[51.5074, -0.0901]} />
+            </MarkerClusterGroup>
+
+
+            {/* <Marker position={position}>
               <Tooltip permanent>
                 <span>Hello...</span>
               </Tooltip>
               <Popup>
                 <span>Ash<br/>S.</span>
               </Popup>
-            </Marker>
+            </Marker> */}
         </Map>
     )
 }
