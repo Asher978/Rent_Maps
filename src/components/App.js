bbe68384e9fdc442c0187c81bc34413da6fe2028
asher978 <ashershaheen@gmail.com>
2018-04-07 21:36:36 -0400

    redirection logic changed to a function

--- src/components/App.js
+++ src/components/App.js
@@ -49,7 +49,7 @@ class App extends Component {
     
     const response = await axios.post('/api/property/add', { title, bedrooms, rent, address, coordinates })
     console.log("after adding a property", response)
-    if (response.status === 200) { this.setState({ currentPage: 'MAP' }) }
+    if (response.status === 200) this.handleDecidePage("MAP");
 
     // TODO -- Redirect here to the Map page
   }
