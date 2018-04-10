076dba3e2045285ae71adb1efcd1c4834a8fdbf7
asher978 <ashershaheen@gmail.com>
2018-04-09 23:40:52 -0400

    New Landing Page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,130 @@
 import React, { Component } from 'react';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
+import Divider from 'material-ui/Divider';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Image from '../assets/main.jpg';
+import GreyMap from '../assets/grey_map.svg';
+import Landlord from '../assets/landlord.svg';
+import House from '../assets/light_grey_house.svg';
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  hero: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: '25rem',
+    backgroundImage: `url(${Image})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+  },
+  heroText: {
+    color: 'black',
+    marginTop: '5rem',
+    fontSize: '2.5rem',
+    fontWeight: 500
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: '8rem',
+    marginTop: '.6rem'
+  },
+  card: {
+    maxWidth: 345,
+    marginTop: '1rem',
+  },
+  media: {
+    height: 100,
+    width: 100,
+    margin: '0 auto'
+  },
+  cardText: {
+    color: '#24C6DC',
+  }
+});
 
 class Landing extends Component {
 
   render () {
+    const { classes } = this.props;
     return (
-      <div>
-        <div className="hero">
-          <Typography>
-            See Actual Rent Data Across San Francisco & New York
-          </Typography>          
-        </div>
+      <div className="heo">
+        <Grid item xs={12}>
+          <Paper className={classes.hero}>
+            <Typography className={classes.heroText}>
+              See Actual Rent Data Across San Francisco & New York
+            </Typography>
+          </Paper>
+        </Grid>
+        <Divider light />
+        <Grid container justify="center" align="center" spacing={24}>
+          <Grid item xs={4}>
+          <Card raised className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image={Landlord}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom className={classes.cardText} variant="headline" component="h2">
+                Leverage
+              </Typography>
+              <Typography component="p">
+              The rental market in San Francisco is heavily weighted in favor of property owners. Prospective tenants are expected to silently accept listing prices.
+              </Typography>
+            </CardContent>
+          </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+          <Card raised className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image={House}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography className={classes.cardText} gutterBottom variant="headline" component="h2">
+                Insight
+              </Typography>
+              <Typography component="p">
+              Without knowing what your neighbors are actually paying, the realator has complete leverage over the tenant. Are forever rising rent prices the result of poor price transparency?
+              </Typography>
+            </CardContent>
+          </Card>
+          </Grid>
+
+          <Grid item xs={4}>
+          <Card raised className={classes.card}>
+            <CardMedia
+              className={classes.media}
+              image={GreyMap}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom className={classes.cardText} variant="headline" component="h2">
+                Transparency
+              </Typography>
+              <Typography component="p">
+              SF Rent Index is an attempt to foster price transparency in San Francisco's rental market without sacraficing the anonimity of its users. Everyone benefits if everyone participates.
+              </Typography>
+            </CardContent>
+          </Card>
+          </Grid>
+        </Grid>
       </div>
       
     )
   }
 }
 
-export default withStyles()(Landing)
+export default withStyles(styles)(Landing)
