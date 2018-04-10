87fb021242416c221eedc9c871227adcc3b15732
asher978 <ashershaheen@gmail.com>
2018-04-09 23:41:28 -0400

    Added home btn

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,6 +7,7 @@ import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
 import AddCircle from 'material-ui-icons/AddCircle';
 import ZoomOutMap from 'material-ui-icons/ZoomOutMap';
+import Home from 'material-ui-icons/Home';
 
 const styles = theme => ({
   root: {
@@ -19,6 +20,11 @@ const styles = theme => ({
   menuButton: {
     marginLeft: -12,
     marginRight: 20
+  },
+  navBar: {
+    background: '#24C6DC',
+    background: '-webkit-linear-gradient(to top, #514A9D, #24C6DC)',
+    background: 'linear-gradient(to top, #514A9D, #24C6DC)',
   }
 });
 
@@ -27,18 +33,21 @@ class Nav extends Component {
   render () {
     const { classes, handleDecidePage } = this.props;
     return (
-      <AppBar position="static" elevation={0}>
+      <AppBar position="static" elevation={0} className={classes.navBar}>
         <Toolbar>
           <IconButton className={classes.menuButton} color="default" onClick={this.props.toggleDrawer}><MenuIcon/></IconButton>
           <Typography className={classes.flex} type="title" color="inherit">
             Rent Maps
           </Typography>
           <div>
+            <IconButton color="default" onClick={() => handleDecidePage("LAND")}>
+              <Home />
+            </IconButton>
             <IconButton color="default" onClick={() => handleDecidePage("ADD")}>
-              <AddCircle/>
+              <AddCircle />
             </IconButton>
             <IconButton color="default" onClick={() => handleDecidePage("MAP")}>
-              <ZoomOutMap/>
+              <ZoomOutMap />
             </IconButton>
           </div>
         </Toolbar>
